7b0f778cfbf0ac97b077e01c6abafce47f753fb5
Aaron Norby <aaron.norby@gmail.com>
2015-12-04 15:18:24 -0800

    Add cancel click handler to form
    This also breaks the form-handlers out into own js file

---
+++ js/form-handler.js
@@ -0,0 +1,50 @@
+(function() {
+  // enable adding a book to the list via input form 
+  var form = document.getElementById('add-book');
+
+  function addBook(e) {
+    e.preventDefault();
+
+    var titleInput = document.getElementById('book-title');
+    var authorInput = document.getElementById('author-name');
+
+    title = titleInput.value;
+    author = authorInput.value;
+
+    // reset the form 
+    titleInput.value = '';
+    authorInput.value = '';
+
+    // check to see if the book is already in the system
+    for (var i = 0; i < window.bookData.books.length; i++) {
+      if (window.bookData.books[i].title === title || window.bookData.books[i].author === author) {
+        alert('It appears you\'ve read this book before. No need to enter it again');
+        return;
+      }
+    }
+
+    // add new book to bookData 
+    window.bookData.books.push({title: title, author: author});
+
+    // generate a new card and add it to the page
+    window.generateBookCard({author: author, title: title});
+
+  }
+  
+  form.addEventListener('submit', addBook);
+
+  // hide form on cancel 
+  function cancelAdd(e) {
+    // hide the form
+    var formContainer = document.getElementsByClassName('input-container')[0];
+    formContainer.classList.remove('form-revealed');
+
+    // hide the overlay 
+    var overlay = document.getElementsByClassName('overlay')[0];
+    overlay.classList.remove('form-revealed');
+  }
+
+  var cancelBtn = document.getElementsByClassName('cancel-btn')[0];
+  cancelBtn.addEventListener('click', cancelAdd);
+
+})();
