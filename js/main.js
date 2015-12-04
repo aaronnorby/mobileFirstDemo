af3a602a8975741d7123d113af57f4e57ee61354
Aaron Norby <aaron.norby@gmail.com>
2015-12-03 21:31:24 -0800

    Add general function to create new cards and append to DOM
    Creates cards from data objects and appends to container div. Will
    need to break out component creators from the monolithic function.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-function generateWelcomeTemplate() {
-  return true;
+// break these inner things out for reuse
+
+function generateCard(book) {
+  var card = document.createElement('section');
+  card.classList.add('card');
+  card.classList.add('book-card');
+
+  var cardContent = document.createElement('section');
+  cardContent.classList.add('card-content');
+
+  //var img = document.createElement('img');
+
+  var title = document.createElement('header');
+  title.classList.add('card-header');
+  title.classList.add('book-title');
+  title.textContent = book.title;
+
+  cardContent.appendChild(title);
+
+  var author = document.createElement('span');
+  author.classList.add('author');
+  author.textContent = book.author;
+
+  cardContent.appendChild(author);
+
+  var cardButtons = document.createElement('div');
+  cardButtons.classList.add('card-buttons');
+
+  var sampleButton = document.createElement('button');
+  sampleButton.classList.add('card-btn', 'sample-btn');
+  sampleButton.textContent = 'Free Sample';
+  cardButtons.appendChild(sampleButton);
+
+  var reviewButton = document.createElement('button');
+  reviewButton.classList.add('card-btn', 'review-btn');
+  reviewButton.textContent = 'Review';
+  cardButtons.appendChild(reviewButton);
+
+  cardContent.appendChild(cardButtons);
+
+  card.appendChild(cardContent);
+
+  var container = document.getElementById('container');
+  container.appendChild(card);
+
 }
+
+generateCard({author: 'Dickens', title: 'what'});
+
