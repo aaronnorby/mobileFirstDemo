76c8febb5e6f2002b190772fd61e7b5a6d4d1284
Aaron Norby <aaron.norby@gmail.com>
2015-12-03 20:18:07 -0800

    Add js object with books to be programmatically added

---
+++ data/data.js
@@ -0,0 +1,12 @@
+var bookData = {
+  books: [
+    {
+      title: 'El Pooch',
+      author: 'Alex Nelson'
+    },
+    {
+      title: 'The Flight',
+      author: 'Scott Masterson'
+    }
+  ]
+}
