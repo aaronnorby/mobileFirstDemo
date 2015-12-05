2bdbb78da67b029d902bfaa83946e7f4d2da234b
Aaron Norby <aaron.norby@gmail.com>
2015-12-04 21:56:34 -0800

    Add ability to reorder by author last name

---
+++ js/reorder-handler.js
@@ -0,0 +1,38 @@
+(function() {
+  function orderByAuthor() {
+    var container = document.getElementById('card-container');
+    var bookCards = document.getElementsByClassName('book-card');
+
+    var oldCards = [];
+    
+    while (bookCards.length) {
+      var removed = container.removeChild(bookCards[0]);
+      oldCards.push(removed);
+    }
+
+    // only sorts by last name;
+    var sorted = oldCards.sort(function(a, b) {
+      // take the last name if it's there, or the single name or one-named authors
+      // (e.g. Madonna). 
+      var aLastName = a.dataset.author.split(' ')[1] || a.dataset.author.split(' ')[0];
+      var bLastName = b.dataset.author.split(' ')[1] || b.dataset.author.split(' ')[0];
+
+      if (aLastName < bLastName) {
+        return -1;
+      } else if (aLastName > bLastName) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    for (var i = 0; i < sorted.length; i++) {
+      container.appendChild(sorted[i]);
+    }
+  }
+
+  // attach the handler 
+  var authOrder = document.getElementsByClassName('auth-order')[0];
+  authOrder.addEventListener('click', orderByAuthor);
+
+})();
